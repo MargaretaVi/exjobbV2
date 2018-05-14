c091c4422971601237c7d545f2e30d11fc8fda8a
Margareta <margareta.vi@gmail.com>
2018-05-14 14:29:11 +0200

    added script to capture webcam feed

---
+++ save_webcame_feed.py
@@ -0,0 +1,30 @@
+import numpy as np
+import cv2
+
+#Change the integer to 0 is it is the build in camera on a laptop, or the first connected USB camera for computers with no built-in webcam.
+cap = cv2.VideoCapture(1) # Capture video from camera
+
+# Get the width and height of frame
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'mp4v') # Be sure to use the lower case
+out = cv2.VideoWriter('output.mp4', fourcc, 10.0, (width, height))
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == True:
+        # write the flipped frame
+        out.write(frame)
+
+        cv2.imshow('frame',frame)
+        if (cv2.waitKey(1) & 0xFF) == ord('q'): # Hit `q` to exit
+            break
+    else:
+        break
+
+# Release everything if job is finished
+out.release()
+cap.release()
+cv2.destroyAllWindows()
